a7145265e47786f74bbe6f76e5d562176c387a63
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-12-15 23:42:41 +0600

    loading loaded

--- src/pages/UsersList/UsersList.js
+++ src/pages/UsersList/UsersList.js
@@ -1,13 +1,25 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Loading from '../Loading/Loading';
 import UsersListShow from '../UsersListShow/UsersListShow';
 
 const UsersList = () => {
-    const[users, setUsers] = useState([]);
-    useEffect(()=>{
-        axios.get('users.json')
-        .then(data => setUsers(data.data))
-    },[])
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/users')
+            .then(data => {
+                setLoading(false);
+                setUsers(data.data);
+            })
+    }, [])
+
+    if(loading){
+        return <Loading></Loading>
+    }
+
+
     return (
         <div className='my-20 grid grid-cols-1 lg:grid-cols-2 lg:mx-64'>
             <div className="card w-96 bg-base-100 shadow-xl mb-8">
@@ -16,18 +28,18 @@ const UsersList = () => {
                     <div className='mt-4'>
                         {
                             users.map(user => <UsersListShow
-                            key={user.id}
-                            user={user}
+                                key={user._id}
+                                user={user}
                             ></UsersListShow>)
                         }
                     </div>
                 </div>
             </div>
             <div className="card w-96 bg-base-100 shadow-xl">
-            <div className="card-body">
-            <h2 className="text-xl font-bold border rounded-lg bg-sky-200 p-2">USERS DETAILS</h2>
+                <div className="card-body">
+                    <h2 className="text-xl font-bold border rounded-lg bg-sky-200 p-2">USERS DETAILS</h2>
+                </div>
             </div>
-        </div>
         </div>
     );
 };
