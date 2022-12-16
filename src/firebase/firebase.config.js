99342d198bcb202b3a4075b522ce96e53c55aba6
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-12-16 12:44:42 +0600

    padding margin update

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUv9CZouuwvBCdOat-okhcwumP-Dk_87A",
+  authDomain: "users-list-50ca1.firebaseapp.com",
+  projectId: "users-list-50ca1",
+  storageBucket: "users-list-50ca1.appspot.com",
+  messagingSenderId: "276923775796",
+  appId: "1:276923775796:web:0e720a289a007a2baa4361"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/pages/UsersListShow/UsersListShow.js
+++ src/pages/UsersListShow/UsersListShow.js
@@ -8,7 +8,7 @@ const UsersListShow = ({ user }) => {
     return (
 
         <Link to={`/users/${_id}`}
-            className=" w-80 mb-4 focus:bg-slate-400 focus:text-white border rounded-lg bg-slate-300 p-2 flex justify-center items-center gap-4">
+            className=" w-80 mb-4 focus:bg-slate-400 focus:text-white border rounded-lg bg-slate-300 p-2 flex  gap-4 mx-auto px-10">
             <div className="avatar">
                 <div className="w-10 rounded-full ring ring-slate-400 ring-offset-base-100 ring-offset-2">
                     {
@@ -19,7 +19,9 @@ const UsersListShow = ({ user }) => {
                     }
                 </div>
             </div>
+            <div className='mt-2'>
             {profile.firstName + ' ' + profile.lastName}
+            </div>
         </Link>
 
     );
